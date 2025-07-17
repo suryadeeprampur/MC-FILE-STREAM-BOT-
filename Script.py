6d4c853026b1a082d702c652699e0401c7f5cd66
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-07-17 13:33:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -254,7 +254,7 @@ Nᴀᴍᴇ - {}"""
     CAPTION = (
         "<b>[ @MOVIECLUB9999 ]</b>\n"
         "<b>[ @MC_MOVIES_HD ]</b>\n"
-        "<b>{file_name}</b>\n\n"
+        "<b>{title}</b>\n\n"
         "<b>🎭 Jᴏɪɴ :- @MC_MOVIES_HD</b>\n"
         "<b>💠 Rᴇǫᴜᴇsᴛ Gʀᴏᴜᴘ :- @MOVIECLUB9999</b>"
     )
