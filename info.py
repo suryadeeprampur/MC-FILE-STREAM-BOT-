06cc7f156673dfb97b756c5f97ba7bf4a0b5d327
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-07-17 11:40:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -77,7 +77,7 @@ PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) #
 # If PREMIUM_AND_REFERAL_MODE is True Then Fill Below Variable, If Flase Then No Need To Fill.
 REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '20')) # number of referal count
 REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1month') # time in week, day, month.
-PAYMENT_QR = environ.get('PAYMENT_QR', 'https://graph.org/file/ce1723991756e48c35aa1.jpg') # payment code picture url.
+PAYMENT_QR = environ.get('PAYMENT_QR', '') # payment code picture url.
 PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>'
 '- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟᴀɴs -\n\n'
 '- 30ʀs - 1 ᴡᴇᴇᴋ\n'
